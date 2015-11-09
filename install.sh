b057bf5157888d8305449c84b76a3a0c9e0c9144
vsoch <vsochat@stanford.edu>
2015-11-09 15:20:15 -0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-'''
-install.sh
 
-This is the single script that the user should run after generating this
-directory to generate all inputs for the pipeline. The only input argument
-is a project directory of interest, WORK. If the application
-is deployed to a virtual machine (either local or on aws) these
-installation steps will not be necessary. User instructions will
-be provided in documentation to give details to this.
+# install.sh
 
-'''
+# This is the single script that the user should run after generating this
+# directory to generate all inputs for the pipeline. The only input argument
+# is a project directory of interest, WORK. If the application
+# is deployed to a virtual machine (either local or on aws) these
+# installation steps will not be necessary. User instructions will
+# be provided in documentation to give details to this.
+
 
 # VARIABLES FILLED IN FROM DEEPDIVE-PYTHON
 WORK=$1
