3f3e68ba686952b9802c3757b7b8ac2adeab5a4f
vsoch <vsochat@stanford.edu>
2015-11-09 16:44:11 -0800

    modified:   run.py

--- run.py
+++ run.py
@@ -21,4 +21,4 @@ download_nltk()
 make_plugin_folders(analysis_dir)
 init_scripts(scripts_directory,analysis_dir)
 
-print "Scripts are generated in scripts folder. Extractions of terms, corpus, and relationships can be done in parallel, but must be done before run_analysis_setup.job [NOT YET WRITTEN] and before run_inference.job [NOT YET WRITTEN]"
+print "Scripts are generated in scripts folder. Use run_slurm.py (and check that the parameters are ok) to submit slurm jobs as follows:\n\n  python run_slurm.py run_terms_extractions.py\n\nLaunch is recommended as another method, see: https://github.com/vsoch/poldracklab-launch\n\nExtractions of terms, corpus, and relationships can be done in parallel, but must be done before run_analysis_setup.job [NOT YET WRITTEN] and before run_inference.job [NOT YET WRITTEN]"
